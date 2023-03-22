31f8139698f4d476774096349dfb9fc7def26754
Vladimir <flin007@inbox.ru>
2023-03-22 04:13:57 +0300

    lesson5

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/webhook', function (){
+    return response('OK', 200);
+});
+
+Route::get('/send', function (){
+    $token = "6176741474:AAG9nuq9y7KQV3XlvDE_slLAQnII34sKzpY";
+    $chatId = 264493118;
+    $text = 'Hello world';
+    $data = http_build_query([
+        'text' => $text,
+        'chat_id' => $chatId,
+    ]);
+    $url = "https://api.telegram.org/bot$token/sendMessage?{$data}";
+    file_get_contents($url);
+
+    return response('OK', 200);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
